a1e8487310aa8a76c22ce41606ff9ee9ce6771c8
Nataliia <kirikovich@i.ua>
2023-04-24 00:57:20 +0200

    updateHW6

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,16 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsList = document.querySelector("#ingredients");
+
+const elementsList = ingredients.map(ingredient => {
+  const ingrList = document.createElement('li');
+  ingrList.classList.add('item');
+  ingrList.textContent = ingredient;
+
+  return ingrList;
+});
+
+ingredientsList.append(...elementsList);
+console.log(ingredientsList);
